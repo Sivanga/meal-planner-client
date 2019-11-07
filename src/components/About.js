0d5d1820d12e35f4630b6f24a15adeac4cf48694
sivanga <sivangal2@gmail.com>
2019-11-07 10:27:50 +0800

    Removed image from above

--- src/components/About.js
+++ src/components/About.js
@@ -37,58 +37,52 @@ const About = () => {
           </div>
         </div>
         <div className="col-12 col-md-5">
-          <img
-            src={SpicesImage}
-            alt="SpicesImage"
-            style={{ width: "100%", height: "auto" }}
-          />
-        </div>
-      </div>
+          <div className="card card-cascade wider">
+            <div className="view view-cascade overlay">
+              <img
+                className="card-img-top"
+                src={SivanGalamidiImage}
+                alt="SivanGalamidiImage"
+              />
+              <a href="#!">
+                <div className="mask rgba-white-slight"></div>
+              </a>
+            </div>
 
-      <div className="card card-cascade wider">
-        <div className="view view-cascade overlay">
-          <img
-            className="card-img-top"
-            src={SivanGalamidiImage}
-            alt="SivanGalamidiImage"
-          />
-          <a href="#!">
-            <div className="mask rgba-white-slight"></div>
-          </a>
-        </div>
-
-        <div className="card-body card-body-cascade text-center pb-0">
-          <h4 className="card-title">
-            <strong>Created by:</strong>
-          </h4>
-          <h5 className="blue-text pb-2">
-            <strong>Sivan Galamidi Saitowitz</strong>
-          </h5>
-          <p className="card-text">
-            Software Developer and a Mom of hungry twins!
-          </p>
-          <div className="social">
-            <a
-              className="px-2 fa-lg li-ic fa-sm"
-              href="https://www.linkedin.com/in/sivan-galamidi-00006258/"
-              target="_blank"
-            >
-              <i className="fab fa-linkedin-in"></i>
-            </a>
-            <a
-              className="px-2 fa-lg tw-ic fa-sm"
-              href="https://github.com/Sivanga"
-              target="_blank"
-            >
-              <i className="fab fa-github"></i>
-            </a>
-            <a
-              className="px-2 fa-lg fb-ic fa-sm"
-              href="https://www.facebook.com/sivan.galamidi"
-              target="_blank"
-            >
-              <i className="fab fa-facebook-f"></i>
-            </a>
+            <div className="card-body card-body-cascade text-center pb-0">
+              <h4 className="card-title">
+                <strong>Created by:</strong>
+              </h4>
+              <h5 className="blue-text pb-2">
+                <strong>Sivan Galamidi Saitowitz</strong>
+              </h5>
+              <p className="card-text">
+                Software Developer and a Mom of hungry twins!
+              </p>
+              <div className="social">
+                <a
+                  className="px-2 fa-lg li-ic fa-sm"
+                  href="https://www.linkedin.com/in/sivan-galamidi-00006258/"
+                  target="_blank"
+                >
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+                <a
+                  className="px-2 fa-lg tw-ic fa-sm"
+                  href="https://github.com/Sivanga"
+                  target="_blank"
+                >
+                  <i className="fab fa-github"></i>
+                </a>
+                <a
+                  className="px-2 fa-lg fb-ic fa-sm"
+                  href="https://www.facebook.com/sivan.galamidi"
+                  target="_blank"
+                >
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
